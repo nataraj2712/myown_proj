3ae903395cbac74538e887ec9296c3cf17e484fe
nataraj2712 <33884458+nataraj2712@users.noreply.github.com>
2018-07-04 23:19:00 +0530

    Add files via upload

--- merge_docx.py
+++ merge_docx.py
@@ -33,7 +33,23 @@ def extract(myFile):
         fileCont = fo.read()
 
     fileCont=re.sub(r'(<w:del [^>]+>)<w:r( [^>]+>((?:(?!</w:r>).)*)</w:r>)','\g<1><w:changed_r\g<2>',fileCont,re.DOTALL)
-    fileCont=re.sub(r'<w:t xml:space="preserve">','<w:t>mergepreservespace',fileCont)
+
+    for mt in re.finditer('<w:t(?: [^>]+)?>((?:(?!</w:t>).)*)</w:t>',fileCont):
+        fullLine=mt.group()
+        bk_fullLine = fullLine
+        grpStr = mt.group(1)
+
+        if grpStr == " ":
+            continue
+        if not re.search('\s',grpStr):
+            continue
+
+        bk_grpStr = grpStr
+        grpStr=re.sub('\s','mergespace',grpStr)
+        fullLine=re.sub(re.escape(bk_grpStr),grpStr,fullLine)
+        fileCont=fileCont.replace(bk_fullLine,fullLine)
+
+
 
     for mt in re.finditer('<w:pPr><w:pStyle w:val="[^"]+"/>((?:(?!</w:pPr>).)*)</w:pPr><w:r[^>]*>((?:(?!<w:t[^>]*>).)*)<w:t[^>]*>', fileCont):
         mt1 = re.search('(<w:([^>]+) w:val="(?:0|baseline|none)"/>)', str(mt.group(1)))
@@ -46,8 +62,7 @@ def extract(myFile):
             if mt1 and str(mt1.group(1)).upper() != "WIDOWCONTROL":
                 fileCont=re.sub(str(mt.group()),str(mt.group(1))+str(mt.group(3))+"mergedocx_start"+(str(mt1.group()).replace("<",'&lt;')).replace('>',"&gt;")+"mergedocx_end",fileCont,count=1,flags=re.DOTALL)
 
-
-
+    fileCont=re.sub(r'(<[^>]+)mergespace([^>]*>)','\g<1> \g<2>',fileCont)
     fileCont=re.sub(r'<w:changed_r([^>]+)>','<w:r\g<1>>',fileCont)
     fo = open(xmlFile,"w")
     fo.write(fileCont)
@@ -59,7 +74,7 @@ def extract(myFile):
 
     return 0
 
-def final(myFile):
+def final(myFile,Extra=False):
     global fileCont
     fo=open(myFile+".log","w")
     fo.close()
@@ -69,40 +84,36 @@ def final(myFile):
         fileCont = fo.read()
 
     fileCont = re.sub(r'(<w:del [^>]+>)<w:r( [^>]+>((?:(?!</w:r>).)*)</w:r>)', '\g<1><w:changed_r\g<2>', fileCont,re.DOTALL)
-    fileCont = re.sub(r'<w:t>mergepreservespace', '<w:t xml:space="preserve">', fileCont)
-    fileCont = re.sub(r'mergepreservespace', '', fileCont)
-    # fileCont = re.sub('</w:t></w:r><w:r><w:rPr><w:rStyle w:val="[^>]+"/></w:rPr><w:lastRenderedPageBreak/><w:t>w:','w:',fileCont)
     fileCont=re.sub('(mergedocx_start&lt;w:[^ ]+ )</w:t>(?:(?:(?!<w:t[^>]*>).)*)<w:t[^>]*>(w:val="[^"]+"/&gt;mergedocx_end)','\g<1>\g<2>',fileCont)
 
-    cnt = 1
     while fileCont.count("mergedocx_start") != 0:
         # </w:rPr><w:commentReference w:id="223"/></w:r><w:r><w:t xml:space="preserve
         for mt in re.finditer('(</w:rPr>(?:<w:[^>/<]+/>)?(?:</w:r><w:r ?[^>]*>)?<w:t[^>]*>(?:(?:(?!</w:t>).)*))mergedocx_start((?:(?!mergedocx_end).)*)mergedocx_end',fileCont):
             fileCont=re.sub(re.escape(mt.group()),(str(mt.group(2).replace("&lt;","<").replace("&gt;",">")))+mt.group(1),fileCont,count=1)
-            # cnt = cnt + 1
-            # print "loop1--->" + str(cnt)
-            # fo = open("D:\\121212.xml", "w")
-            # fo.write(fileCont)
-            # fo.close()
+
 
         # </w:pPr><w:r w:rsidRPr="006F3A49"><w:t>
         # </w:pPr><w:r><w:sym w:font="Wingdings" w:char="F0D8"/></w:r><w:r><w:t
         for mt in re.finditer('(</w:pPr>(?:<w:r ?[^>]*><w:[^>]+></w:r>)?(?:<w:[^>/<]+/>)?<w:r ?[^>]*>(?:<w:lastRenderedPageBreak/>)?<w:t[^>]*>(?:(?:(?!</w:t>).)*))mergedocx_start((?:(?!mergedocx_end).)*)mergedocx_end',fileCont):
             fileCont = re.sub(re.escape(mt.group()),'<w:rPr>'+(str(mt.group(2).replace("&lt;", "<").replace("&gt;", ">"))) + '</w:rPr>'+mt.group(1), fileCont, count=1)
-            # cnt = cnt + 1
-            # print "loop3--->" + str(cnt)
-            # fo = open("D:\\121212.xml", "w")
-            # fo.write(fileCont)
-            # fo.close()
-        # fo = open("D:\\121212.xml", "w")
-        # fo.write(fileCont)
-        # fo.close()
+
 
 
     fileCont = re.sub(r'<w:changed_r([^>]+)>', '<w:r\g<1>>', fileCont)
-    # fileCont = re.sub(r'/&gt;&lt;w:', '/><w:', fileCont)
-    # fileCont = re.sub(r'(<w:r [^>]+>(?:<w:lastRenderedPageBreak/>)?)(<w:t[^>]*>)mergedocx_start&lt;((?:(?!&gt;mergedocx_end).)*)&gt;mergedocx_end',
-    #     '\g<1><w:rPr><\g<3>></w:rPr>\g<2>', fileCont)
+
+    if Extra==True:
+        for mt_extra in re.finditer(r'<w:p [^>]*>((?:(?!</w:p>).)*)</w:p>', fileCont, flags=re.DOTALL):
+            txtGrp = str(mt_extra.group(0))
+            bktxtGrp = txtGrp
+            mt1_extra = re.findall(
+                '<w:r( [^>]*)?><w:rPr><w:rFonts[^>]+w:ascii="Symbol"[^>]*/></w:rPr><w:t>((?:(?!</w:r>).)*)</w:r>',
+                txtGrp, flags=re.DOTALL)
+            if len(mt1_extra) > 1:
+                txtGrp = re.sub(
+                    '<w:r( [^>]*)?><w:rPr><w:rFonts[^>]+w:ascii="Symbol"[^>]*/></w:rPr><w:t>((?:(?!</w:r>).)*)</w:r>',
+                    '', txtGrp, count=len(mt1_extra) - 1)
+                fileCont = fileCont.replace(bktxtGrp, txtGrp)
+
     fo = open(xmlFile, "w")
     fo.write(fileCont)
     fo.close()
@@ -115,7 +126,9 @@ def final(myFile):
 
 IpFile = sys.argv[1]
 function2Exec = sys.argv[2]
-if function2Exec == "extract":
+if function2Exec=="extract":
     extract(IpFile)
-if function2Exec == "final":
-    final(IpFile)
+if function2Exec=="final":
+    final(IpFile,Extra=False)
+if function2Exec=="final_extra":
+    final(IpFile,Extra=True)
